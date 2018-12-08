7f45cefe852f2a51a5144055193afd12fcc0beba
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2018-12-08 21:10:14 +0330

    Adjust Difficulty of PoW

--- Blockchain/Blockchain.go
+++ Blockchain/Blockchain.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/golang/protobuf/jsonpb"
 
@@ -22,9 +25,47 @@ import (
 )
 
 type Blockchain struct {
-	Tip *msg.Packet
-	DB  *leveldb.DB
+	Tip       *msg.Packet
+	DB        *leveldb.DB
+	chainLock sync.Mutex
+	//tmp debug
+	minRetargetTimespan int64  // target timespan / adjustment factor
+	maxRetargetTimespan int64  // target timespan * adjustment factor
+	blocksPerRetarget   uint64 // target timespan / target time per block
+	chainParams         *chainParams
 }
+
+type chainParams struct {
+	CoinbaseMaturity         uint16
+	TargetTimespan           time.Duration
+	TargetTimePerBlock       time.Duration
+	RetargetAdjustmentFactor int64
+	ReduceMinDifficulty      bool
+	MinDiffReductionTime     time.Duration
+	GenerateSupported        bool
+}
+
+//tmp debug
+
+func (b *Blockchain) InitBlockchain() {
+	params := chainParams{
+		CoinbaseMaturity:         100,
+		TargetTimespan:           time.Hour * 24 * 14, // 14 days
+		TargetTimePerBlock:       time.Minute * 10,    // 10 minutes
+		RetargetAdjustmentFactor: 2,                   // 25% less, 400% more
+		ReduceMinDifficulty:      false,
+		MinDiffReductionTime:     0,
+		GenerateSupported:        false,
+	}
+	targetTimespan := int64(params.TargetTimespan / time.Second)         //TargetTimespan in seconds
+	targetTimePerBlock := int64(params.TargetTimePerBlock / time.Second) //TargetTimePerBlock in seconds
+	adjustmentFactor := params.RetargetAdjustmentFactor
+	b.blocksPerRetarget = uint64(targetTimespan / targetTimePerBlock) //2016
+	b.minRetargetTimespan = targetTimespan / adjustmentFactor         // 1209600÷4= 302400
+	b.maxRetargetTimespan = targetTimespan * adjustmentFactor         // 1209600x4= 4838400
+	b.chainParams = &params
+}
+
 type BlockchainIterator struct {
 	Tip msg.Packet
 	DB  *leveldb.DB
@@ -130,6 +171,8 @@ func GenesisBlock() *msg.Packet {
 // AddBlock add new block to current blockchain
 // Tip: validation should get done before hand
 func (b *Blockchain) AddBlock(p *msg.Packet) error {
+	b.chainLock.Lock()
+	// TODO: change the expected target
 	db := b.DB
 	raw, _ := proto.Marshal(p)
 	err := db.Put(
@@ -143,6 +186,7 @@ func (b *Blockchain) AddBlock(p *msg.Packet) error {
 		return err
 	}
 	b.Tip = p
+	b.chainLock.Unlock()
 	return nil
 }
 
@@ -207,6 +251,9 @@ func (b *Blockchain) ExportToJSON(path string) error {
 	defer f.Close()
 	buf := bytes.NewBuffer([]byte{})
 	_, err = buf.WriteString("[")
+	if err != nil {
+		return err
+	}
 	m := jsonpb.Marshaler{}
 	iter := &BlockchainIterator{}
 	err = iter.InitIter(b)
@@ -244,4 +291,71 @@ func (b *Blockchain) ExportToJSON(path string) error {
 		return err
 	}
 	return nil
+}
+
+func (b *Blockchain) ReadBlock(hash []byte) (*msg.Packet, error) {
+	db := b.DB
+	rawBlock, err := db.Get(bytes.Join(
+		[][]byte{[]byte("b"), hash}, []byte{}),
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	block := &msg.Packet{}
+	err = proto.Unmarshal(rawBlock, block)
+	if err != nil {
+		return nil, err
+	}
+	switch block.Data.(type) {
+	case *msg.Packet_BlockData:
+		return block, nil
+	default:
+		panic(errors.New("wrong Packet type as last block"))
+	}
+}
+
+func (b *Blockchain) Ancestor(height uint64) (*msg.Packet, error) {
+	if height < 1 || height > b.Tip.CurrentBlockNumber {
+		return nil, Consts.ErrWrongHeight
+	}
+	n := b.Tip
+	for ; n != nil && n.CurrentBlockNumber != height; n, _ = b.ReadBlock(n.Prev) {
+		// Intentionally left blank
+	}
+	return n, nil
+}
+
+func (b *Blockchain) RelativeAncestor(p *msg.Packet, distance uint64) (*msg.Packet, error) {
+	return b.Ancestor(p.CurrentBlockNumber - distance)
+}
+
+func (b *Blockchain) CalcNextRequiredDifficulty() (uint32, error) {
+	b.chainLock.Lock()
+	if (b.Tip.CurrentBlockNumber+1)%b.NextRetarget() != 0 {
+		b.chainLock.Unlock()
+		return b.Tip.Diff, nil
+	}
+	fmt.Println("string")
+	firstNode, err := b.RelativeAncestor(b.Tip, b.blocksPerRetarget-2)
+	if err != nil {
+		return 0, Consts.ErrRetargetRetriv
+	}
+	fmt.Println("string")
+	actualTimespan := b.Tip.Timestamp.Seconds - firstNode.Timestamp.Seconds
+	adjustedTimespan := actualTimespan
+	if actualTimespan < b.minRetargetTimespan {
+		adjustedTimespan = b.minRetargetTimespan
+	} else if actualTimespan > b.maxRetargetTimespan {
+		adjustedTimespan = b.maxRetargetTimespan
+	}
+	targetTimeSpan := int64(b.chainParams.TargetTimespan / time.Second)
+	diff := int64(b.Tip.Diff) * (targetTimeSpan / adjustedTimespan)
+	b.chainLock.Unlock()
+	fmt.Printf("diff :%d\n", diff)
+	return uint32(diff), nil
+}
+
+func (b *Blockchain) NextRetarget() uint64 {
+	retarget := b.blocksPerRetarget - (b.Tip.CurrentBlockNumber % b.blocksPerRetarget)
+	return retarget + b.Tip.CurrentBlockNumber
 }
