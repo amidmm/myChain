4bd1cd0d44bd51e2167b51c87abbda56ece23ee1
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2018-12-08 21:08:45 +0330

    Added new errors

--- Consts/Consts.go
+++ Consts/Consts.go
@@ -12,4 +12,6 @@ var (
 	ErrBlockchainExists = errors.New(Name + " blockchain exists")
 	ErrWrongTarget      = errors.New("wrong target value")
 	ErrCanceled         = errors.New("calculation canceled")
+	ErrWrongHeight      = errors.New("height limitation exceeded")
+	ErrRetargetRetriv   = errors.New("unable to obtain previous retarget block")
 )
