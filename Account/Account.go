e727d0874b35c27afaf537c7944885d86a330e8a
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2018-12-11 22:42:16 +0330

    Added account generation and load

---
+++ Account/Account.go
@@ -0,0 +1,107 @@
+package Account
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	mrand "math/rand"
+	"sync"
+
+	"github.com/amidmm/MyChain/Consts"
+	"github.com/syndtr/goleveldb/leveldb/storage"
+
+	"github.com/syndtr/goleveldb/leveldb"
+
+	"github.com/libp2p/go-libp2p-crypto"
+)
+
+type User struct {
+	Name     string
+	PubKey   crypto.PubKey
+	PrivKey  crypto.PrivKey
+	userLock sync.Mutex
+}
+
+func CreateUser(name string, pub crypto.PubKey, priv crypto.PrivKey, debug bool, debugSeed int64) error {
+	u := &User{}
+	u.userLock.Lock()
+	defer u.userLock.Unlock()
+	u.Name = name
+	if debug {
+		if err := u.setKey(debugSeed); err != nil {
+			return err
+		}
+	} else if pub != nil && priv != nil {
+		u.PubKey = pub
+		u.PrivKey = priv
+	} else {
+		u.setKey(0)
+	}
+	path, err := storage.OpenFile(fmt.Sprintf(Consts.UserDB, name), false)
+	if err != nil {
+		return err
+	}
+	defer path.Close()
+	db, err := leveldb.Open(path, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	raw, err := crypto.MarshalPublicKey(u.PubKey)
+	if err != nil {
+		return err
+	}
+	db.Put([]byte("pub"), raw, nil)
+	raw, err = crypto.MarshalPrivateKey(u.PrivKey)
+	if err != nil {
+		return err
+	}
+	db.Put([]byte("priv"), raw, nil)
+	return nil
+}
+
+func (u *User) setKey(seed int64) error {
+	var r io.Reader
+	if seed != 0 {
+		r = mrand.New(mrand.NewSource(int64(seed)))
+	} else {
+		r = rand.Reader
+	}
+	priv, pubKey, err := crypto.GenerateKeyPairWithReader(crypto.ECDSA, 1024, r)
+	if err != nil {
+		return err
+	}
+	u.PrivKey = priv
+	u.PubKey = pubKey
+	return nil
+}
+
+func LoadUser(name string) (*User, error) {
+	u := &User{}
+	u.Name = name
+	path, err := storage.OpenFile(fmt.Sprintf(Consts.UserDB, name), false)
+	if err != nil {
+		return nil, err
+	}
+	defer path.Close()
+	db, err := leveldb.Open(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	rawPub, err := db.Get([]byte("pub"), nil)
+	if err != nil {
+		return nil, err
+	}
+	if u.PubKey, err = crypto.UnmarshalPublicKey(rawPub); err != nil {
+		return nil, err
+	}
+	rawPriv, err := db.Get([]byte("priv"), nil)
+	if err != nil {
+		return nil, err
+	}
+	if u.PrivKey, err = crypto.UnmarshalPrivateKey(rawPriv); err != nil {
+		return nil, err
+	}
+	return u, nil
+}
