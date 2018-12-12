ef14abf82cc53647468185aa4bceb594f1290d37
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2018-12-12 20:20:08 +0330

    Added Sign of Packet

--- Packet/Packet.go
+++ Packet/Packet.go
@@ -1,10 +1,34 @@
 package Packet
 
 import (
+	"github.com/amidmm/MyChain/Account"
 	"github.com/amidmm/MyChain/Consts"
 	"github.com/amidmm/MyChain/Messages"
+	"github.com/golang/protobuf/proto"
 )
 
 func GetPacket(hash []byte) (*msg.Packet, error) {
 	return &msg.Packet{}, Consts.ErrNotImplemented
+}
+
+func SetPacketSign(p *msg.Packet, u Account.User) error {
+	sign, err := GetPacketSign(p, u)
+	if err != nil {
+		return err
+	}
+	p.Sign = sign
+	return nil
+}
+
+func GetPacketSign(p *msg.Packet, u Account.User) ([]byte, error) {
+	p.Sign = nil
+	raw, err := proto.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+	sign, err := u.PrivKey.Sign(raw)
+	if err != nil {
+		return nil, err
+	}
+	return sign, nil
 }
