0778f9ca880145203fd4459fd256d11826671725
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2018-12-12 20:24:29 +0330

    Added sign tx and verify it

--- Transaction/Transaction.go
+++ Transaction/Transaction.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"fmt"
 
+	"github.com/libp2p/go-libp2p-crypto"
+
+	"github.com/amidmm/MyChain/Account"
+
 	"github.com/amidmm/MyChain/Consts"
 	"github.com/amidmm/MyChain/Messages"
 	"github.com/golang/protobuf/proto"
@@ -29,8 +33,8 @@ func ValidateTx(t *msg.Tx, coinbase bool) (bool, error) {
 func GetTxHash(t *msg.Tx) []byte {
 	exBundleHash := t.BundleHash
 	exHash := t.Hash
-	t.BundleHash = []byte{}
-	t.Hash = []byte{}
+	t.BundleHash = nil
+	t.Hash = nil
 	raw, _ := proto.Marshal(t)
 	hash := sha3.New512()
 	hash.Write(raw)
@@ -39,6 +43,46 @@ func GetTxHash(t *msg.Tx) []byte {
 	return hash.Sum(nil)
 }
 
+func SetTxSign(t *msg.Tx, u *Account.User) error {
+	t.Sign = nil
+	t.BundleHash = nil
+	raw, err := proto.Marshal(t)
+	if err != nil {
+		return err
+	}
+	sign, err := u.PrivKey.Sign(raw)
+	if err != nil {
+		return err
+	}
+	t.Sign = sign
+	return nil
+}
+
 func ValidateTxSign(t *msg.Tx) (bool, error) {
-	return true, Consts.ErrNotImplemented
+	sign := t.Sign
+	BundleHash := t.BundleHash
+	t.Sign = nil
+	t.BundleHash = nil
+	defer func() {
+		t.Sign = sign
+		t.BundleHash = BundleHash
+	}()
+	refTx, err := GetUTXO(t.RefTx)
+	pub, err := crypto.UnmarshalPublicKey(refTx.Sign)
+	if err != nil {
+		return false, err
+	}
+	raw, err := proto.Marshal(t)
+	if err != nil {
+		return false, err
+	}
+	if v, err := pub.Verify(raw, sign); err != nil || !v {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
+
+func GetUTXO(hash []byte) (*msg.Tx, error) {
+	return nil, Consts.ErrNotImplemented
 }
