dff2f3c07c523a78040b595de98f75214d59946f
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2019-01-05 15:32:25 +0330

    Added Sign Verification and JSON export, fix user ref bug

--- Packet/Packet.go
+++ Packet/Packet.go
@@ -1,18 +1,21 @@
 package Packet
 
 import (
+	"github.com/golang/protobuf/jsonpb"
+
 	"github.com/amidmm/MyChain/Account"
 	"github.com/amidmm/MyChain/Consts"
 	"github.com/amidmm/MyChain/Messages"
 	"github.com/go-ethereum/crypto/sha3"
 	"github.com/golang/protobuf/proto"
+	"github.com/libp2p/go-libp2p-crypto"
 )
 
 func GetPacket(hash []byte) (*msg.Packet, error) {
 	return &msg.Packet{}, Consts.ErrNotImplemented
 }
 
-func SetPacketSign(p *msg.Packet, u Account.User) error {
+func SetPacketSign(p *msg.Packet, u *Account.User) error {
 	sign, err := GetPacketSign(p, u)
 	if err != nil {
 		return err
@@ -21,8 +24,16 @@ func SetPacketSign(p *msg.Packet, u Account.User) error {
 	return nil
 }
 
-func GetPacketSign(p *msg.Packet, u Account.User) ([]byte, error) {
+func GetPacketSign(p *msg.Packet, u *Account.User) ([]byte, error) {
 	p.Sign = nil
+	nonce := p.Nonce
+	hash := p.Hash
+	p.Nonce = nil
+	p.Hash = nil
+	defer func() {
+		p.Nonce = nonce
+		p.Hash = hash
+	}()
 	raw, err := proto.Marshal(p)
 	if err != nil {
 		return nil, err
@@ -53,4 +64,36 @@ func GetHash(p msg.Packet) ([]byte, error) {
 	}
 	hash.Write(raw)
 	return hash.Sum(nil), nil
+}
+
+func ValidateSign(p *msg.Packet) (bool, error) {
+	userPK, err := crypto.UnmarshalPublicKey(p.Addr)
+	if err != nil {
+		return false, err
+	}
+	sign := p.Sign
+	nonce := p.Nonce
+	hash := p.Hash
+	p.Sign = nil
+	p.Nonce = nil
+	p.Hash = nil
+	defer func() {
+		p.Sign = sign
+		p.Nonce = nonce
+		p.Hash = hash
+	}()
+	raw, err := proto.Marshal(p)
+	if err != nil {
+		return false, err
+	}
+	if ok, err := userPK.Verify(raw, sign); err != nil || !ok {
+		return false, err
+	}
+	return true, nil
+}
+
+func JSON(p *msg.Packet) string {
+	json := jsonpb.Marshaler{}
+	s, _ := json.MarshalToString(p)
+	return s
 }
