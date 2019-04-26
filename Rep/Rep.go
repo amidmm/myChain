2b55e3d3ee90fae3ff3d8b22a6a3f93e795c9feb
Seyed Amid Moeinzadeh Mirhosseini <amid.moeinzadeh@gmail.com>
2019-04-26 20:51:45 +0430

    [+] POPR and POPRA validation

--- Rep/Rep.go
+++ Rep/Rep.go
@@ -1,13 +1,19 @@
 package Rep
 
 import (
+	"bytes"
+	"encoding/gob"
+	"errors"
 	"log"
 
 	"github.com/go-ethereum/crypto/sha3"
 	"github.com/golang/protobuf/proto"
 
+	"github.com/amidmm/MyChain/Tangle"
+
 	"github.com/amidmm/MyChain/Consts"
 	"github.com/amidmm/MyChain/Messages"
+	crypto "github.com/libp2p/go-libp2p-crypto"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/storage"
 )
@@ -27,6 +33,93 @@ func init() {
 		}
 		RepPool = db
 	}
+}
+
+func ValidateRep(p *msg.Packet, t *Tangle.Tangle) (bool, error) {
+	if p.GetRepData() == nil {
+		return false, Consts.ErrWrongParam
+	}
+	if !bytes.Equal(p.GetRepData().Hash, GetHash(p.GetRepData())) {
+		return false, errors.New("wrong hash for rep")
+	}
+	switch p.GetRepData().RepType {
+	case msg.Rep_POPR:
+		if p.GetRepData().GetPOPRData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+		if p.GetRepData().GetPOPRData().Certified != nil {
+			var certRaw bytes.Buffer
+			enc := gob.NewEncoder(&certRaw)
+			enc.Encode(p.GetRepData().Nonce)
+			enc.Encode(p.Addr)
+			enc.Encode(p.Timestamp)
+			pub, err := crypto.UnmarshalPublicKey(p.GetRepData().GetPOPRData().Certified.CertAddr)
+			if err != nil {
+				return false, err
+			}
+			result, _ := pub.Verify(certRaw.Bytes(), p.GetRepData().GetPOPRData().Certified.CertSign)
+			if result {
+				return true, nil
+			}
+			if p.GetRepData().Nonce == nil {
+				return false, errors.New("wrong nonce")
+			}
+			return false, nil
+		}
+		return true, nil
+
+	case msg.Rep_POPRA:
+		if p.GetRepData().GetPOPRAData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+		poprRaw, err := t.DB.Get(bytes.Join(
+			[][]byte{[]byte("b"), p.GetRepData().Ref}, []byte{}),
+			nil)
+		if err != nil {
+			return false, nil
+		}
+		popr := &msg.Packet{}
+		proto.Unmarshal(poprRaw, popr)
+		if popr.PacketType != msg.Packet_REP || popr.GetRepData().RepType != msg.Rep_POPR {
+			return false, errors.New("wrong ref")
+		} else if !bytes.Equal(popr.GetRepData().Addr, p.GetRepData().Addr) {
+			return false, errors.New("wrong ref")
+		}
+		if !bytes.Equal(p.GetRepData().Nonce, popr.GetRepData().Nonce) {
+			return false, errors.New("wrong nonce")
+		}
+		return true, nil
+
+	case msg.Rep_AGREE:
+		if p.GetRepData().GetAgreeData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+
+	case msg.Rep_CANCEL:
+		if p.GetRepData().GetCancelData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+	case msg.Rep_COMPLAINT:
+		if p.GetRepData().GetComplaintData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+	case msg.Rep_MEDIATOR:
+		if p.GetRepData().GetMediatorData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+	case msg.Rep_REVIEW:
+		if p.GetRepData().GetReviewData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+
+	case msg.Rep_REVENGE:
+		if p.GetRepData().GetRevengeData() == nil {
+			return false, Consts.ErrWrongParam
+		}
+	default:
+		return false, Consts.ErrWrongParam
+	}
+	return false, nil
 }
 
 func GetHash(rep *msg.Rep) []byte {
